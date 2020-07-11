f142a9af6d311fd344f094c4ce6ee5a2a6d68154
etesami <eh.etesami@gmail.com>
2020-07-10 20:51:05 -0400

    Changed the main.py to argument based file (run-study.py)

--- run-study.py
+++ run-study.py
@@ -1,50 +1,65 @@
-from federated_learning.FederatedLearning import FederatedLearning
-import logging
-import torch
-from FLNet import FLNet
+"""
+Usage: run-study.py [--avg | --opt] --attack=<attack-type> --mal-workers=<list-of-uesr-ids> --mal-worker-percentage=<woker-percentage> --data-percentage=<data-percentage> --output-file=<output-filename>
 
-'''
-01_avg: average, normal situation
-02_opt: wieghted average, normal situation
-03_att1_avg: average, attack 1
-04_att1_opt: wieghted average, attack 1
-'''
+Options:
+  --avg    Average mode
+  --opt    Optimized mode
+  -attck   Attack ID
+  --mal-workers             List of ids indication malicious workers
+  --mal-worker-percentage   Percentage of users
+  --data-percentage  use right-hand side
+  --output-file
+"""
+from docopt import docopt
+# from federated_learning.FederatedLearning import FederatedLearning
+# import logging
+# import torch
+# from FLNet import FLNet
+arguments = docopt(__doc__)
+print(arguments)
 
-OUTPUT_PATH = "/home/savi/FederatedLearning/data_tmp/04_att1_opt"
-MNIST_PATH = "/home/savi/data/MNIST"
-WORKERS_NUM = 10
-EPOCH_NUM = 5
+# '''
+# 01_avg: average, normal situation
+# 02_opt: wieghted average, normal situation
+# 03_att1_avg: average, attack 1
+# 04_att1_opt: wieghted average, attack 1
+# '''
 
-fl = FederatedLearning(
-        workers_num = WORKERS_NUM, 
-        epochs_num = EPOCH_NUM, 
-        output_prefix = OUTPUT_PATH,
-        mnist_path = MNIST_PATH, 
-        log_level = logging.DEBUG)
+# OUTPUT_PATH = "/home/savi/FederatedLearning/data_tmp/04_att1_opt"
+# MNIST_PATH = "/home/savi/data/MNIST"
+# WORKERS_NUM = 10
+# EPOCH_NUM = 5
 
-fl.create_workers()
-fl.create_server()
-fl.load_data()
+# fl = FederatedLearning(
+#         workers_num = WORKERS_NUM, 
+#         epochs_num = EPOCH_NUM, 
+#         output_prefix = OUTPUT_PATH,
+#         mnist_path = MNIST_PATH, 
+#         log_level = logging.DEBUG)
 
-fl.attack_premute_labels([0])
+# fl.create_workers()
+# fl.create_server()
+# fl.load_data()
 
-server_data_loader = fl.create_aggregated_data()
-train_data_loader, test_data_loader = fl.create_datasets()
+# fl.attack_premute_labels([0])
 
-fl.create_server_model()
-fl.create_workers_model()
+# server_data_loader = fl.create_aggregated_data()
+# train_data_loader, test_data_loader = fl.create_datasets()
 
-for epoch in range(1, EPOCH_NUM + 1):
-    fl.train_server(server_data_loader, epoch)
-    fl.train_workers(train_data_loader, epoch)
-    print()
-    W = fl.find_best_weights(epoch)
-#     W = [0.1] * 10
+# fl.create_server_model()
+# fl.create_workers_model()
 
-    # base model is meant nothing in this scenario
-    fl.update_models(W, fl.server_model, fl.workers_model)
+# for epoch in range(1, EPOCH_NUM + 1):
+#     fl.train_server(server_data_loader, epoch)
+#     fl.train_workers(train_data_loader, epoch)
+#     print()
+#     W = fl.find_best_weights(epoch)
+# #     W = [0.1] * 10
 
-    # Apply the server model to the test dataset
-    fl.test(fl.server_model, test_data_loader, epoch)
+#     # base model is meant nothing in this scenario
+#     fl.update_models(W, fl.server_model, fl.workers_model)
+
+#     # Apply the server model to the test dataset
+#     fl.test(fl.server_model, test_data_loader, epoch)
 
 
