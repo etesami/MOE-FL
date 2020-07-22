c4a36766ca2c1de5743b6f65441ca635af73df68
etesami <eh.etesami@gmail.com>
2020-07-21 23:01:04 -0400

    Update test output accuract (adjust " position)

--- federated_learning/FederatedLearning.py
+++ federated_learning/FederatedLearning.py
@@ -350,7 +350,7 @@ class FederatedLearning():
                 correct += pred.eq(target.view_as(pred)).sum().item()
 
         test_loss /= len(test_loader.dataset)
-        TO_FILE = '{} {} "{{/*0.80 Accuracy:}}\\n{}% {}"\n'.format(
+        TO_FILE = '{} {} "{{/*0.80 Accuracy:}}\\n{}%" {}\n'.format(
             epoch, test_loss, 
             100. * correct / len(test_loader.dataset),
             100. * correct / len(test_loader.dataset))
